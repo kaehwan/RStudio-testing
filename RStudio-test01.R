a8336e15b1d4d2184bc1fa1b8d2b37d842b0478f
kaehwan <love01007@hotmail.com>
2020-05-16 12:53:01 +0800

    six common verbs in dplyr

--- RStudio-test01.R
+++ RStudio-test01.R
@@ -1,3 +1,14 @@
 print("This file was created within RStudio")
 
 print("And now it is live on Github")
+
+pacman::p_load(pacman, tidyverse, rio)
+
+# Six common verbs in dplyr -----------------------------------------------
+
+# select() - picks columns based on their names
+# filter() - picls rows based on their values
+# group_by() - groups the data based on one/ more variables
+# summarize() - reduced multiple values down to a single summary
+# mutate() - adds new columns using existing columns
+# arrange() - changes the ordering of the rows
